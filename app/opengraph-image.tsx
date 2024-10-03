f0cc4869d7b4c8e67b62e4a4807fafa6ceeb45a0
chideraemmanuel <120053997+chideraemmanuel@users.noreply.github.com>
2024-10-03 01:02:49 +0100

    chore: update og image font size

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -18,7 +18,7 @@ export default function Image() {
           flexDirection: 'column',
           backgroundImage:
             'radial-gradient(circle at 10% 20%, rgb(222, 168, 248) 0%, rgb(168, 222, 248) 21.8%, rgb(189, 250, 205) 35.6%, rgb(243, 250, 189) 52.9%, rgb(250, 227, 189) 66.8%, rgb(248, 172, 172) 90%, rgb(254, 211, 252) 99.7%)',
-          fontSize: 60,
+          fontSize: 100,
           letterSpacing: -2,
           fontWeight: 700,
           textAlign: 'center',
